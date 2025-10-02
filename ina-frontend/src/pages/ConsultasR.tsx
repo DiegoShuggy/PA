eb524db8c20af234f1bcc62f4c8c0fabc76515ce
Jorge Arias Caffi <jorg.ariasc@duocuc.cl>
2025-10-02 18:47:27 -0300

    añadido ruta a chat por cada área de consultas

--- ina-frontend/src/pages/ConsultasR.tsx
+++ ina-frontend/src/pages/ConsultasR.tsx
@@ -1,18 +1,40 @@
+import { Link } from 'react-router-dom';
 import './ConsultasR.css';
 
 function ConsultasR() {
   console.log('ConsultasR component is rendering'); // ← Agrega esto
-  
+
   return (
     <div className="consultas-r" >
       <h2>Áreas de consultas</h2>
-      <p className='uno'>Asuntos Estudiantil</p>
-      <p className='dos'>Desarrollo Profesional y Titulados</p>
-      <p className='tres'>Bienestar Estudiantil</p>
-      <p className='cuatro'>Deportes</p>
-      <p className='cinco'>Pastoral</p>
+      <div className="area">
+        <Link to="/InA">
+          <p className='uno'>Asuntos Estudiantiles</p>
+          </Link>
+      </div>
+      <div className="area">
+        <Link to="/InA">
+          <p className='dos'>Desarrollo Profesional y Titulados</p>
+        </Link>
+      </div>
+      <div className="area">
+        <Link to="/InA">
+          <p className='tres'>Bienestar Estudiantil</p>
+        </Link>
+      </div>
+      <div className="area">
+        <Link to="/InA">
+          <p className='cuatro'>Deportes</p>
+        </Link>
+      </div>
+      <div className="area">
+        <Link to="/InA">
+          <p className='cinco'>Pastoral</p>
+        </Link>
+      </div>
     </div>
   );
 }
+
 
 export default ConsultasR;
