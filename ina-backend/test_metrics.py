b0c253ad4b68851e1838dc36439abbb6c9a9dd47
Diego Julio Pinto Beltran <127121667+DiegoShuggy@users.noreply.github.com>
2025-10-22 18:11:20 -0300

    Limpiando archivos

--- ina-backend/test_metrics.py
+++
@@ -1,7 +0,0 @@
-from app.metrics_tracker import metrics_tracker
-
-metrics_tracker.track_response_time("test", 0.5, "demo")
-metrics_tracker.log_cache_hit()
-metrics_tracker.log_user_feedback(3)
-
-print(metrics_tracker.get_performance_stats())
